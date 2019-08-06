ab2281a41e9fca20d0e6cc646fb942a9064d1f53
Billy Todd <billy.todd50@yahoo.com>
2019-08-06 14:43:54 -0700

    hoisting

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// functions
+// function declarations can be hoisted to top. declared at bottom of script.
+calculateAge(1999);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+};
+
+retirement(1990);
 
 
+// variables
+var age = 23;
+console.log(age);
 
-
-
-
-
-
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
